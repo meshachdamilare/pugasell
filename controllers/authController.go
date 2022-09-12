fc3679b04af437a953fb9655da1b983f71fde0eb
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-12 23:05:40 +0100

    initialized auth controller

---
+++ controllers/authController.go
@@ -0,0 +1,15 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
