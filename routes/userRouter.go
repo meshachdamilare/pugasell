46eb3e1947c618f938775432b438f88f5c392ad9
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-12 23:31:52 +0100

    initialized usercontroller.go

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	controller "github.com/Christomesh/pugasell/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(router *gin.Engine) {
+	user := router.Group("/api/v1/user")
+	{
+		user.GET("/", controller.GetAllUsers())
+		user.GET("/showme", controller.ShowCurrentUser())
+		user.GET("/:id", controller.GetSingleUser())
+		user.PATCH("/updateuser", controller.UpdateUser())
+		user.PATCH("/updatepassword", controller.UpdateUserPassword())
+	}
+}
