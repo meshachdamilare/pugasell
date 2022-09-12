1a4f22e1324616ffe53086a2cdf90b4f36a30087
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-12 17:17:15 +0100

    Initializing the server

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome to PUGASELL"})
+	})
+
+	router.Run(":" + port)
 
 }
