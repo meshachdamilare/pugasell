63c1cdecc61375b2e79aec2eea2c13114fc41a37
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-12 23:32:19 +0100

    ...

---
+++ controllers/userController.go
@@ -0,0 +1,33 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetAllUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetSingleUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func ShowCurrentUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateUserPassword() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
