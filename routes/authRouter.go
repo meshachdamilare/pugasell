93d745fe72a2586cdc28999ed91c1a2d22915322
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-12 23:10:12 +0100

    renamed authRoute to authRouter

---
+++ routes/authRouter.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	controller "github.com/Christomesh/pugasell/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(router *gin.Engine) {
+	auth := router.Group("/api/v1")
+	{
+		auth.POST("/register", controller.Signup())
+		auth.POST("/login", controller.Login())
+	}
+
+}
