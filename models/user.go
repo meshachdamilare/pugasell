27c90d446d7cd731125005524a60d2dfe9a3c2e5
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-13 15:30:48 +0100

    fixed

--- models/user.go
+++ models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
 	ID         primitive.ObjectID `bson:"_id"`
@@ -10,4 +14,7 @@ type User struct {
 	Avatar     string             `json:"avatar"`
 	Password   string             `json:"password" validate:"min=6"`
 	Role       string             `json:"role" default:"USER" validate:"required,ADMIN|USER"`
+	User_id    string             `json:"user_id"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
 }
