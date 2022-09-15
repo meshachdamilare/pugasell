7665101f7845789aa048f87aeb2e40b6b7103c5d
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-15 16:14:42 +0100

    fixed

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -10,8 +10,8 @@ func UserRoutes(router *gin.Engine) {
 	user := router.Group("/api/v1/users")
 	{
 		user.GET("/", middleware.AuthenticateUser(), middleware.AuthorizedPermissions("ADMIN"), controller.GetAllUsers())
-		user.GET("/showme", middleware.AuthenticateUser(), controller.ShowCurrentUser())
-		user.GET("/:id", middleware.AuthenticateUser(), controller.GetSingleUser())
+		user.GET("/showMe", middleware.AuthenticateUser(), controller.ShowCurrentUser())
+		user.GET("/:userId", middleware.AuthenticateUser(), controller.GetSingleUser())
 		user.PATCH("/updateuser", middleware.AuthenticateUser(), controller.UpdateUser())
 		user.PATCH("/updatepassword", middleware.AuthenticateUser(), controller.UpdateUserPassword())
 	}
