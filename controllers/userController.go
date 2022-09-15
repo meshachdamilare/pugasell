37d2c80166f0c9da53552ee3328bdc0ebbb7dc6a
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-15 16:13:37 +0100

    added showCurrentUser endpoint

--- controllers/userController.go
+++ controllers/userController.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"time"
 
+	middleware "github.com/Christomesh/pugasell/middleware"
+	"github.com/Christomesh/pugasell/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -26,38 +28,64 @@ func GetAllUsers() gin.HandlerFunc {
 		// 	page = 1
 		// }
 		// skip := (page - 1) * limit
+
 		length, err := Usercollection.CountDocuments(ctx, bson.M{"role": "USER"})
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
 			return
 		}
 
-		// to exclude the password field and avatar field from the result obtained and response object
-		opts := options.Find().SetProjection(bson.D{{Key: "password", Value: 0}, {Key: "avatar", Value: 0}})
+		// to exclude the password field from the result obtained and response object
+		opts := options.Find().SetProjection(bson.D{{Key: "password", Value: 0}})
 
 		cursor, err := Usercollection.Find(ctx, bson.M{"role": "USER"}, opts)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
 			return
 		}
-		var allusers []bson.M
-		if err := cursor.All(ctx, &allusers); err != nil {
+
+		var userResponse []models.UserResponseModel
+		if err := cursor.All(ctx, &userResponse); err != nil {
 			log.Fatal(err)
 		}
 
-		c.JSON(http.StatusOK, gin.H{"users": allusers, "counts": length})
+		c.JSON(http.StatusOK, gin.H{"users": userResponse, "counts": length})
 	}
 }
 
 func GetSingleUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		userId := c.Param("userId")
+		var userResponse models.UserResponseModel
 
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		if err := middleware.CheckPermission(c, userId); err != nil {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Not Authorized to access this route"})
+			return
+		}
+
+		opts := options.FindOne().SetProjection(bson.D{{Key: "password", Value: 0}})
+		err := Usercollection.FindOne(ctx, bson.M{"user_id": userId}, opts).Decode(&userResponse)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"user": userResponse})
 	}
 }
 
 func ShowCurrentUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var user = make(map[string]string)
 
+		user["userId"] = c.GetString("userId")
+		user["email"] = c.GetString("email")
+		user["role"] = c.GetString("role")
+		c.JSON(http.StatusOK, gin.H{"success": user})
 	}
 }
 
