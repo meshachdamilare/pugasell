8ac0c399b5d3d37f1bd45fc23dc77987006959e3
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-15 16:14:27 +0100

    added userResponse model

--- models/user.go
+++ models/user.go
@@ -19,6 +19,18 @@ type User struct {
 	Updated_at time.Time          `json:"updated_at"`
 }
 
+type UserResponseModel struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	First_name string             `json:"first_name" validate:"required,min=2,max=50"`
+	Last_name  string             `json:"last_name" validate:"required,min=2,max=50"`
+	Email      string             `json:"email" validate:"required,email"`
+	Avatar     string             `json:"avatar"`
+	Role       string             `json:"role" default:"USER" validate:"omitempty,eq=ADMIN|eq=USER"`
+	User_id    string             `json:"user_id"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+}
+
 type LoginModel struct {
 	Email    string `json:"email" validate:"email,required"`
 	Password string `json:"password" validate:"required,min=6"`
