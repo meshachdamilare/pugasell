82c13549e1aaae0ee30fc89188d84acddc1ab925
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-15 11:40:14 +0100

    added Get all users functionality

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,10 +1,51 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func GetAllUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		// limit, err := strconv.Atoi(c.Query("limit"))
+		// if err != nil || limit < 1 {
+		// 	limit = 2
+		// }
+
+		// page, err := strconv.Atoi(c.Query("page"))
+		// if err != nil || page < 1 {
+		// 	page = 1
+		// }
+		// skip := (page - 1) * limit
+		length, err := Usercollection.CountDocuments(ctx, bson.M{"role": "USER"})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+			return
+		}
+
+		// to exclude the password field and avatar field from the result obtained and response object
+		opts := options.Find().SetProjection(bson.D{{Key: "password", Value: 0}, {Key: "avatar", Value: 0}})
+
+		cursor, err := Usercollection.Find(ctx, bson.M{"role": "USER"}, opts)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+			return
+		}
+		var allusers []bson.M
+		if err := cursor.All(ctx, &allusers); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{"users": allusers, "counts": length})
 	}
 }
 
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,16 +2,17 @@ package routes
 
 import (
 	controller "github.com/Christomesh/pugasell/controllers"
+	middleware "github.com/Christomesh/pugasell/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(router *gin.Engine) {
-	user := router.Group("/api/v1/user")
+	user := router.Group("/api/v1/users")
 	{
-		user.GET("/", controller.GetAllUsers())
-		user.GET("/showme", controller.ShowCurrentUser())
-		user.GET("/:id", controller.GetSingleUser())
-		user.PATCH("/updateuser", controller.UpdateUser())
-		user.PATCH("/updatepassword", controller.UpdateUserPassword())
+		user.GET("/", middleware.AuthenticateUser(), middleware.AuthorizedPermissions("ADMIN"), controller.GetAllUsers())
+		user.GET("/showme", middleware.AuthenticateUser(), controller.ShowCurrentUser())
+		user.GET("/:id", middleware.AuthenticateUser(), controller.GetSingleUser())
+		user.PATCH("/updateuser", middleware.AuthenticateUser(), controller.UpdateUser())
+		user.PATCH("/updatepassword", middleware.AuthenticateUser(), controller.UpdateUserPassword())
 	}
 }
