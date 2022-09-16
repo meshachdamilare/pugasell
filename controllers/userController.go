1aa0c88ec9f7bd40368744e0180f901665442c32
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-16 11:24:42 +0100

    added updateUserProfile controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -8,8 +8,10 @@ import (
 
 	middleware "github.com/Christomesh/pugasell/middleware"
 	"github.com/Christomesh/pugasell/models"
+	util "github.com/Christomesh/pugasell/utils"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -81,6 +83,9 @@ func GetSingleUser() gin.HandlerFunc {
 func ShowCurrentUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var user = make(map[string]string)
+		//get userId, email, role  of logged-in user via details stored in the cookie
+		// These can be used to get more info about the user from the database
+		// And display them if neccessary
 
 		user["userId"] = c.GetString("userId")
 		user["email"] = c.GetString("email")
@@ -91,12 +96,135 @@ func ShowCurrentUser() gin.HandlerFunc {
 
 func UpdateUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var userUpdate models.UpdateUserModel
+		var foundUser models.User
+
+		if err := c.BindJSON(&userUpdate); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if validateErr := validate.Struct(userUpdate); validateErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validateErr.Error()})
+			return
+		}
+		//get userId of logged in user via details stored in the cookie
+		userId := c.GetString("userId")
+		role := c.GetString("role")
+
+		var updateObj primitive.D
+
+		if userUpdate.First_name != "" {
+			updateObj = append(updateObj, bson.E{Key: "first_name", Value: userUpdate.First_name})
+
+		}
+		if userUpdate.Last_name != "" {
+			updateObj = append(updateObj, bson.E{Key: "last_name", Value: userUpdate.Last_name})
+
+		}
+		if userUpdate.Email != "" {
+			updateObj = append(updateObj, bson.E{Key: "email", Value: userUpdate.Email})
+
+		}
+
+		err := Usercollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&foundUser)
+		if err != nil {
+			msg := "message:User was not found"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		updateObj = append(updateObj, bson.E{Key: "avatar", Value: userUpdate.Avatar})
+		userUpdate.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{Key: "updated_at", Value: userUpdate.Updated_at})
+
+		uspsert := true
+		filter := bson.M{"user_id": userId}
+
+		opt := options.UpdateOptions{
+			Upsert: &uspsert,
+		}
+
+		result, err := Usercollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{Key: "$set", Value: updateObj},
+			},
+			&opt,
+		)
+		if err != nil {
+			msg := "user item update failed"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		util.GenerateToken(c, userUpdate.Email, userId, role)
+		c.JSON(http.StatusOK, gin.H{"message": "Successfully updated", "result": result.ModifiedCount})
 
 	}
 }
 
-func UpdateUserPassword() gin.HandlerFunc {
-	return func(c *gin.Context) {
+// func UpdateUserPassword() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+// 		defer cancel()
 
-	}
-}
+// 		var changePassword models.ChangePasswordModel
+// 		var foundUser models.User
+
+// 		if err := c.BindJSON(&changePassword); err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 			return
+// 		}
+// 		if validateErr := validate.Struct(changePassword); validateErr != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": validateErr.Error()})
+// 			return
+// 		}
+
+// 		userId := c.GetString("userId")
+
+// 		var updateObj primitive.D
+
+// 		err := Usercollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&foundUser)
+// 		if err != nil {
+// 			msg := "message:User was not found"
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+// 			return
+// 		}
+// 		isPasswordCorrect, msg := util.VerifyPassword(changePassword.OldPassword, foundUser.Password)
+// 		if !isPasswordCorrect {
+// 			c.JSON(http.StatusUnauthorized, gin.H{"error": msg})
+// 		}
+
+// 		if changePassword.NewPassword != "" {
+// 			newPassword := util.HashPassword(changePassword.NewPassword)
+// 			updateObj = append(updateObj, bson.E{Key: "password", Value: newPassword})
+
+// 		}
+
+// 		uspsert := true
+// 		filter := bson.M{"user_id": userId}
+
+// 		opt := options.UpdateOptions{
+// 			Upsert: &uspsert,
+// 		}
+
+// 		result, err := Usercollection.UpdateOne(
+// 			ctx,
+// 			filter,
+// 			bson.D{
+// 				{Key: "$set", Value: updateObj},
+// 			},
+// 			&opt,
+// 		)
+// 		if err != nil {
+// 			msg := "failed to change password"
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+// 			return
+// 		}
+
+// 		c.JSON(http.StatusOK, gin.H{"message": "Password successfully changed", "result": result.ModifiedCount})
+// 	}
+// }
