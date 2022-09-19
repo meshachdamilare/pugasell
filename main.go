7c7c4177f078f8d07a0626d61de599c77c409ba6
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-19 08:43:31 +0100

    updated

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 
 	routes.AuthRoutes(router)
 	routes.UserRoutes(router)
+	routes.ProductRoutes(router)
 
 	router.GET("/api/v1", middleware.AuthenticateUser(), middleware.AuthorizedPermissions("ADMIN"), func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Welcome to PUGASELL"})
