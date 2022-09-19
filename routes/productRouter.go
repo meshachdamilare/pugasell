d905503e3e87251f20ac26fcca52f27f5aa39ff3
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-19 18:17:13 +0100

    updated

--- routes/productRouter.go
+++ routes/productRouter.go
@@ -10,10 +10,14 @@ func ProductRoutes(router *gin.Engine) {
 	product := router.Group("/api/v1/products")
 	{
 		product.GET("/", controller.GetAllProducts())
-		product.POST("/", middleware.AuthenticateUser(), middleware.AuthorizedPermissions("ADMIN"), controller.CreateProduct())
-		product.POST("/uploadImage", middleware.AuthenticateUser(), controller.UploadImage())
-		product.GET("/:product_id", middleware.AuthenticateUser(), controller.GetSingleProduct())
-		product.PATCH("/:product_id", middleware.AuthenticateUser(), controller.UpdateProduct())
-		product.DELETE("/:product_id", middleware.AuthenticateUser(), controller.DeleteProduct())
+		product.GET("/:product_id", controller.GetSingleProduct())
+
+		// ONly admin can access this resource
+		product.Use(middleware.AuthenticateUser(), middleware.AuthorizedPermissions("ADMIN"))
+
+		product.POST("/", controller.CreateProduct())
+		product.POST("/uploadImage", controller.UploadImage())
+		product.PATCH("/:product_id", controller.UpdateProduct())
+		product.DELETE("/:product_id", controller.DeleteProduct())
 	}
 }
