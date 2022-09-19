4a781dca2c4449f3f05f1192634dcade31ea5537
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-19 18:38:48 +0100

    added review routes

---
+++ routes/reviewRouter.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	controller "github.com/Christomesh/pugasell/controllers"
+	middleware "github.com/Christomesh/pugasell/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func ReviewRoutes(router *gin.Engine) {
+	review := router.Group("/api/v1/reviews")
+	{
+		review.GET("/:review_id", controller.GetSingleReview())
+		review.GET("/", controller.GetAllReviews())
+
+		// ONly logged-in users can access this resource
+		review.Use(middleware.AuthenticateUser())
+
+		review.POST("/", controller.CreateProduct())
+		review.PATCH("/:review_id", controller.UpdateReview())
+		review.DELETE("/:review_id", controller.DeleteProduct())
+
+	}
+}
