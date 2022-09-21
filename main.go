6d175e54eb4ff78cff04158e5e1f81e5240d88a3
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-21 13:01:00 +0100

    added order route to main

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	routes.UserRoutes(router)
 	routes.ProductRoutes(router)
 	routes.ReviewRoutes(router)
+	routes.OrderRoutes(router)
 
 	router.GET("/api/v1", middleware.AuthenticateUser(), middleware.AuthorizedPermissions("ADMIN"), func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Welcome to PUGASELL"})
